3d5b01e7b02aa9242dec82a5d28a5b845c2ecb8c
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-06-10 16:38:25 +0200

    u4-w2-d3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.time.LocalDate;
+import java.util.List;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<Product> products = List.of(
+                new Product("Libro Java", "Books", 120.0),
+                new Product("Pannolini Pampers", "Baby", 30.0),
+                new Product("Macchinina", "Boys", 50.0),
+                new Product("Romanzo Fantasy", "Books", 80.0),
+                new Product("Felpa Ragazzi", "Boys", 40.0)
+        );
+
+
+        List<Customer> customers = List.of(
+                new Customer("Mario Rossi", 1),
+                new Customer("Luca Bianchi", 2),
+                new Customer("Anna Verdi", 2)
+        );
+
+        List<Order> orders = List.of(
+                new Order(
+                        "DELIVERED",
+                        LocalDate.of(2021, 2, 10),
+                        LocalDate.of(2021, 2, 15),
+                        List.of(products.get(0), products.get(2)),
+                        customers.get(1)
+                ),
+                new Order(
+                        "PENDING",
+                        LocalDate.of(2021, 3, 5),
+                        LocalDate.of(2021, 3, 12),
+                        List.of(products.get(1)),
+                        customers.get(0)
+                ),
+                new Order(
+                        "DELIVERED",
+                        LocalDate.of(2021, 1, 20),
+                        LocalDate.of(2021, 1, 25),
+                        List.of(products.get(3), products.get(4)),
+                        customers.get(2)
+                )
+        );
+
+List<Product> ex1 = products.stream()
+        .filter(p -> p.getCategory().equalsIgnoreCase("Books"))
+        .filter(p->p.getPrice()>100).toList();
+        System.out.println("\nesercizio 1");
+ex1.forEach(System.out::println);
+
+List<Order> ex2=orders.stream()
+        .filter(order -> order.getProducts()
+                .stream().anyMatch(product -> product.getCategory().equalsIgnoreCase("baby"))).toList();
+        System.out.println("\nesercizio 2");
+        ex2.forEach(System.out::println);
+        List<Product> ex3 = products.stream()
+                .filter(product -> product.getCategory().equalsIgnoreCase("boys"))
+                .map(p -> new Product(
+                        p.getName(),
+                        p.getCategory(),
+                        p.getPrice() * 0.9
+                ))
+                .toList();
+        System.out.println("\nesercizio 3");
+        ex3.forEach(System.out::println);
+        List<Order> ex4 = orders.stream()
+                .filter(order -> order.getCustomer().getTier() == 2)
+                .filter(order -> order.getOrderDate().isAfter(LocalDate.of(2021, 2, 1))
+                        && order.getOrderDate().isBefore(LocalDate.of(2021, 4, 1)))
+                .toList();
+
+        System.out.println("\nesercizio 3");
+        ex4.forEach(System.out::println);
+
+    }
 }
