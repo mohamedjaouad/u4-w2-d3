52faae55c7a1b4ab48bf295d3435e20948a8a445
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-06-10 17:16:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,54 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        List<Product> products = List.of(
-                new Product("Libro Java", "Books", 120.0),
-                new Product("Pannolini Pampers", "Baby", 30.0),
-                new Product("Macchinina", "Boys", 50.0),
-                new Product("Romanzo Fantasy", "Books", 80.0),
-                new Product("Felpa Ragazzi", "Boys", 40.0)
-        );
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Libro Java", "Books", 120.0));
+        products.add(new Product("Pannolini Pampers", "Baby", 30.0));
+        products.add(new Product("Macchinina", "Boys", 50.0));
+        products.add(new Product("Romanzo Fantasy", "Books", 80.0));
+        products.add(new Product("Felpa Ragazzi", "Boys", 40.0));
 
 
-        List<Customer> customers = List.of(
-                new Customer("Mario Rossi", 1),
-                new Customer("Luca Bianchi", 2),
-                new Customer("Anna Verdi", 2)
-        );
 
-        List<Order> orders = List.of(
-                new Order(
-                        "DELIVERED",
-                        LocalDate.of(2021, 2, 10),
-                        LocalDate.of(2021, 2, 15),
-                        List.of(products.get(0), products.get(2)),
-                        customers.get(1)
-                ),
-                new Order(
-                        "PENDING",
-                        LocalDate.of(2021, 3, 5),
-                        LocalDate.of(2021, 3, 12),
-                        List.of(products.get(1)),
-                        customers.get(0)
-                ),
-                new Order(
-                        "DELIVERED",
-                        LocalDate.of(2021, 1, 20),
-                        LocalDate.of(2021, 1, 25),
-                        List.of(products.get(3), products.get(4)),
-                        customers.get(2)
-                )
-        );
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Mario Rossi", 1));
+        customers.add(new Customer("Luca Bianchi", 2));
+        customers.add(new Customer("Anna Verdi", 2));
 
-List<Product> ex1 = products.stream()
+
+        List<Order> orders = new ArrayList<>();
+
+        orders.add(new Order(
+                "DELIVERED",
+                LocalDate.of(2021, 2, 10),
+                LocalDate.of(2021, 2, 15),
+                List.of(products.get(0), products.get(2)),
+                customers.get(1)
+        ));
+
+        orders.add(new Order(
+                "PENDING",
+                LocalDate.of(2021, 3, 5),
+                LocalDate.of(2021, 3, 12),
+                List.of(products.get(1)),
+                customers.get(0)
+        ));
+
+        orders.add(new Order(
+                "DELIVERED",
+                LocalDate.of(2021, 1, 20),
+                LocalDate.of(2021, 1, 25),
+                List.of(products.get(3), products.get(4)),
+                customers.get(2)
+        ));
+
+
+        List<Product> ex1 = products.stream()
         .filter(p -> p.getCategory().equalsIgnoreCase("Books"))
         .filter(p->p.getPrice()>100).toList();
         System.out.println("\nesercizio 1");
